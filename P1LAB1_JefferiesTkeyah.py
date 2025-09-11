4616839a60eba14a414bdf92e1aa74edd5d910a4
tkeyahj <jefferit5864@student.faytechcc.edu>
2025-09-10 20:04:07 -0400

    Add files via upload

---
+++ P1LAB1_JefferiesTkeyah.py
@@ -0,0 +1,8 @@
+# Tkeyah Jefferies
+# September 10, 2025
+# P1LAB1
+# This program asks the user for their first and last name,
+# then displays a welcome message including both names.
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hello, " + first_name + " " + last_name + "! Welcome to CTI-110")
